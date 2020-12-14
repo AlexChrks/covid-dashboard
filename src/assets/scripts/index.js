92e0ca3311ee3595166ee694d2616354c7481c4c
skamok <sk_amok@mail.ru>
2020-12-14 20:50:14 +0300

    feat: add App class and OptionsPanel class

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,7 @@
 import '../styles/reset.css';
 import '../styles/style.css';
+import App from './App.js';
+import Covid19API from './Covid19API.js';
 
 const imagesContext = require.context('../images', true, /.(png|svg)$/);
 const imagesObj = {};
@@ -9,3 +11,9 @@ imagesContext.keys().forEach((key) => {
   imagesObj[code] = imagesContext(key);
 });
 export default imagesObj;
+const lastUpdatedLabel = document.getElementById('lastupdateddate');
+const generalGrid = document.getElementById('generalgrid');
+Covid19API.getSummary().then((summary) => {
+  const app = new App(summary, lastUpdatedLabel, generalGrid);
+  app.init();
+});
