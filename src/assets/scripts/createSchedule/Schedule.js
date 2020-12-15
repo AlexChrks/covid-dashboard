ba521f6b91be6aa53b14411d2c86863eee24d0f6
Mike Pateev <tokensmix@gmail.com>
2020-12-15 13:06:26 +0300

    feat: schdule, generate

--- src/assets/scripts/createSchedule/Schedule.js
+++ src/assets/scripts/createSchedule/Schedule.js
@@ -2,46 +2,151 @@ import Chart from './Chart.bundle.min.js';
 import CovidAPI from '../Covid19API.js';
 
 class Schedule {
-  constructor(ctx) {
-    this.ctx = ctx;
+  constructor() {
+    this.generateSchedule();
+    this.createSchedule();
   }
 
-  set setArr(val) {
-    this.arrayDays = val;
+  generateSchedule() {
+    this.graphWidget = document.querySelector('.graph_widget');
+    this.scheduleInside = document.createElement('div');
+    this.canvas = document.createElement('canvas');
+    this.containerSelect = document.createElement('div');
+
+    this.optionSelect = document.createElement('select');
+    this.paramSelect = document.createElement('select');
+    this.percentSelect = document.createElement('select');
+
+    this.paramCases = document.createElement('option');
+    this.paramDeaths = document.createElement('option');
+    this.paramRecovered = document.createElement('option');
+    this.optionDaily = document.createElement('option');
+    this.optionTotal = document.createElement('option');
+    this.percentAbs = document.createElement('option');
+    this.percentPer100k = document.createElement('option');
+
+    this.paramCases.innerText = 'Cases';
+    this.paramDeaths.innerText = 'Deaths';
+    this.paramRecovered.innerText = 'Recovered';
+    this.optionDaily.innerText = 'Daily';
+    this.optionTotal.innerText = 'Cumulative';
+    this.percentAbs.innerText = 'Absolute';
+    this.percentPer100k.innerText = 'Per 100k';
+
+    this.paramCases.value = 'totalConfirmed';
+    this.paramDeaths.value = 'totalDeaths';
+    this.paramRecovered.value = 'totalRecovered';
+    this.optionDaily.value = 'daily';
+    this.optionTotal.value = 'cumulative';
+    this.percentAbs.value = 'absolute';
+    this.percentPer100k.value = 'per100k';
+
+    this.paramCases.setAttribute('selected', 'selected');
+    this.optionTotal.setAttribute('selected', 'selected');
+    this.percentAbs.setAttribute('selected', 'selected');
+
+    this.containerSelect.className = 'schedule_select';
+    this.scheduleInside.className = 'schedule_inside';
+
+    this.canvas.id = 'myChart';
+    this.optionSelect.id = 'select_option';
+    this.paramSelect.id = 'select_param';
+    this.percentSelect.id = 'select_percent';
+
+    this.ctx = this.canvas.getContext('2d');
+
+    this.optionSelect.append(this.optionDaily, this.optionTotal);
+    this.paramSelect.append(this.paramCases, this.paramDeaths, this.paramRecovered);
+    this.percentSelect.append(this.percentAbs, this.percentPer100k);
+
+    this.containerSelect.append(this.paramSelect, this.optionSelect, this.percentSelect);
+    this.scheduleInside.append(this.canvas);
+    this.graphWidget.append(this.scheduleInside, this.containerSelect);
   }
 
-  createSchedule(country = 'world', option = 'cumulative_totalConfirmed', percent = false) {
-    const selectSchedule = document.getElementById('select_schedule');
+  createSchedule(country = 'world', param = 'totalConfirmed',
+    option = 'cumulative', percent = 'absolute') {
+    this.country = country;
+    const selectParam = document.getElementById('select_param');
+    const selectOption = document.getElementById('select_option');
+    const selectPercent = document.getElementById('select_percent');
+    const arraySelected = [];
+    arraySelected.push(selectParam, selectOption, selectPercent);
     setTimeout(() => {
-      selectSchedule.value = option;
+      selectParam.value = param;
+      selectOption.value = option;
+      selectPercent.value = percent;
     }, 200);
-    if (!percent) {
-      if (country !== 'world') {
-        this.promiseHistory = CovidAPI.getCountryHistory(country)
-          .then((database) => database)
-          .catch((error) => new Error(error.message));
-      } else if (country === 'world' && !percent) {
-        this.promiseHistory = CovidAPI.getWorldHistory()
-          .then((database) => database)
-          .catch((error) => new Error(error.message));
-      }
+    const arrayPromises = [];
+    if (country !== 'world') {
+      this.promiseHistory = CovidAPI.getCountryHistory(country)
+        .then((database) => database)
+        .catch((error) => new Error(error.message));
     } else {
-      
+      this.promiseHistory = CovidAPI.getWorldHistory()
+        .then((database) => database)
+        .catch((error) => new Error(error.message));
     }
-    this.promiseHistory.then((res) => {
-      this.setArr = res;
-      this.drawSchedule(option.split('_')[1], option.split('_')[0]);
+    this.promiseSummary = CovidAPI.getSummary()
+      .then((database) => database)
+      .catch((error) => new Error(error.message));
+    arrayPromises.push(this.promiseSummary, this.promiseHistory);
+    Promise.all(arrayPromises).then((arr) => {
+      [this.objCases, this.arrayDays] = arr;
+      this.drawSchedule(param, option, percent);
     });
     this.handleEvent = (e) => {
       if (e.type === 'change') {
-        const selectScheduleValue = selectSchedule.value;
-        this.drawSchedule(selectScheduleValue.split('_')[1],
-          selectScheduleValue.split('_')[0],
-          selectScheduleValue.split('_')[2]);
+        this.drawSchedule(selectParam.value, selectOption.value, selectPercent.value);
       }
     };
-    selectSchedule.removeEventListener('change', this, false);
-    selectSchedule.addEventListener('change', this, false);
+    arraySelected.forEach((el) => {
+      el.removeEventListener('change', this, false);
+    });
+    arraySelected.forEach((el) => {
+      el.addEventListener('change', this, false);
+    });
+  }
+
+  transformMaxValue(num) {
+    let newNum;
+    this.toStringNum = `${Math.trunc(num)}`;
+    if (num < 1) newNum = 1;
+    else {
+      newNum = (Math.trunc(Math.trunc(num)
+        / (10 ** (this.toStringNum.length - 1))) * 2) * (10 ** (this.toStringNum.length - 1));
+    }
+    return newNum;
+  }
+
+  convertAbsoluteCases(param, option) {
+    let population;
+    if (this.country === 'world') {
+      population = this.objCases.global.totalPopulation;
+    } else {
+      this.objCases.countries.forEach((el) => {
+        if (el.name === this.country) {
+          population = el.population;
+        }
+      });
+    }
+    for (let i = 1; i < this.arrayDays.length; i++) {
+      let num;
+      if (option === 'daily') {
+        num = ((this.arrayDays[i][param] - this.arrayDays[i - 1][param])
+          / population) * (10 ** 5);
+      } else if (option === 'cumulative') {
+        num = (this.arrayDays[i][param] / population) * (10 ** 5);
+      }
+      if (num < 0) num = 0;
+      this.arr.push({
+        x: new Date(`${this.arrayDays[i].date}`),
+        y: (num.toFixed(3))
+      });
+    }
+    const max = Math.max(...this.arr.map((el) => el.y));
+    this.maxValue = this.transformMaxValue(max);
+    this.stepSize = this.maxValue / 4;
   }
 
   convertCases(param, option) {
@@ -58,8 +163,9 @@ class Schedule {
         y: (num)
       });
     }
-    this.maxValue = Math.ceil(Math.max(...this.arr.map((el) => el.y)) / 10) * 15;
-    this.stepSize = this.maxValue / 3;
+    const max = Math.max(...this.arr.map((el) => el.y));
+    this.maxValue = this.transformMaxValue(max);
+    this.stepSize = this.maxValue / 4;
   }
 
   drawSchedule(param, option, percent) {
@@ -70,10 +176,13 @@ class Schedule {
     } else {
       this.color = 'rgb(41, 181, 20)';
     }
-    if (option === 'daily') this.type = 'bar';
-    else if (option === 'cumulative') this.type = 'bubble';
+
+    if (option === 'cumulative') {
+      this.type = 'bubble';
+    } else this.type = 'bar';
     this.arr = [];
-    this.convertCases(param, option);
+    if (percent === 'absolute') this.convertCases(param, option);
+    else this.convertAbsoluteCases(param, option);
     const chartConfig = {
       type: this.type,
       data: {
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,6 +1,5 @@
 import '../styles/reset.css';
 import '../styles/style.css';
-import Dashboard from './createSchedule/createSchedule.js';
 
 const imagesContext = require.context('../images', true, /.(png|svg)$/);
 const imagesObj = {};
@@ -9,19 +8,5 @@ imagesContext.keys().forEach((key) => {
     .substring(0, key.length - 6);
   imagesObj[code] = imagesContext(key);
 });
-
-function createDashboard() {
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const dashboard = new Dashboard(ctx);
-  dashboard.createSchedule();
-  document.querySelector('.map_widget').addEventListener('click', () => {
-    dashboard.createSchedule('Belarus', 'daily_totalDeaths');
-  });
-  document.querySelector('.countries_widget').addEventListener('click', () => {
-    dashboard.createSchedule('world', 'cumulative_totalRecovered');
-  });
-}
-
-createDashboard();
 
 export default imagesObj;
